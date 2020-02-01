d40ac324c00ca00b3231bfd91694d738ca33c1f6
Karl Duckett <karlplduckett@gmail.com>
2020-02-01 21:15:19 +1300

    Template for module selection

--- app.py
+++ app.py
@@ -5,6 +5,5 @@ app = Flask(__name__)
 from views import *
 
 
-
 if __name__ == "__main__":
     app.run(debug=True)
--- views.py
+++ views.py
@@ -4,3 +4,7 @@ from flask import Flask, render_template
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/material_01')
+def material_01():
+    return render_template('material_01.html')
