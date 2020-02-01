ce3d7b5802a9e7f9b7b2e000feaa0deb5dd5d245
Karl Duckett <karlplduckett@gmail.com>
2020-02-01 22:40:44 +1300

    Added animation and load div

--- app.py
+++ app.py
@@ -4,6 +4,5 @@ app = Flask(__name__)
 
 from views import *
 
-
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/material_01.js
+++ static/js/material_01.js
@@ -1,1 +1,25 @@
-console.log("HELLO")
+cards = []
+
+$('.card').hide()
+
+window.onload = function() {
+    document.getElementById("loader").style.display = "none";
+
+    cards = $('.wrapper').children('.card')
+    display_cards()
+};
+
+function display_cards() {
+    for (let i = 0; i < cards.length; i++) {
+        setTimeout(function timer() {
+            console.log(cards[i]);
+            $(cards[i]).css('display', 'flex');
+            $(cards[i]).addClass('fadeInUp animated')
+        }, i * 300);
+    }
+}
+
+$('#close_icon').click(function() {
+    $('#welcome_alert').addClass('fadeOutUp animated')
+    setTimeout("$('#welcome_alert').hide()", 400);
+})
