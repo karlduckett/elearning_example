c03cfed8947dc5f2fa9da12a7477385fc69d446c
Karl Duckett <karlplduckett@gmail.com>
2020-02-02 17:54:46 +1300

    DG Example Pages with Assessment Start

--- static/js/mod_dangerous_goods.js
+++ static/js/mod_dangerous_goods.js
@@ -1,3 +1,56 @@
 window.onload = function() {
     document.getElementById("loader").style.display = "none";
 };
+window.onbeforeunload = function() {
+    return "Leaving this page will reset the wizard";
+};
+
+//START PAGE BTN
+$("#start_btn").click(function() {
+    $("#page_1").hide()
+    $('#page_2').show()
+});
+
+
+//SIDE BAR BTN
+$('#dg_content_holder_p2').hide()
+$('#dg_content_holder_assessment').hide()
+
+$('#dg_p1_btn').click(function() {
+    $('#dg_content_holder_p1').show()
+    $('#dg_content_holder_p2').hide()
+    $('#dg_content_holder_assessment').hide()
+
+    $('.list-group').children('a').each(function() {
+        $(this).removeClass('list-group-item-success') // "this" is the current element in the loop
+    });
+
+    $('#dg_p1_btn').removeClass('list-group-item-light')
+    $('#dg_p1_btn').addClass('list-group-item-success')
+})
+
+$('#dg_p2_btn').click(function() {
+    $('#dg_content_holder_p1').hide()
+    $('#dg_content_holder_assessment').hide()
+    $('#dg_content_holder_p2').show()
+
+    $('.list-group').children('a').each(function() {
+        $(this).removeClass('list-group-item-success') // "this" is the current element in the loop
+    });
+
+    $('#dg_p2_btn').removeClass('list-group-item-light')
+    $('#dg_p2_btn').addClass('list-group-item-success')
+})
+
+$('#dg_p_ass').click(function() {
+    $('#dg_content_holder_p1').hide()
+    $('#dg_content_holder_p2').hide()
+    $('#dg_content_holder_assessment').show()
+
+    $('.list-group').children('a').each(function() {
+        $(this).removeClass('list-group-item-success') // "this" is the current element in the loop
+    });
+
+    $('#dg_p_ass').removeClass('list-group-item-light')
+    $('#dg_p_ass').addClass('list-group-item-success')
+})
